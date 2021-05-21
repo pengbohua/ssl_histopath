ce32e416f9448778f0fe02ad08199cde602d6422
Marvin Peng <35765640+pengbohua@users.noreply.github.com>
2021-05-21 22:48:28 +0800

    Delete tst.sh

--- tst.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PATH=miniconda3/bin/:$PATH
-export PATH=/vol/bitbucket/bohua/miniconda3/bin/:$PATH
-source activate
-CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 test_parallel.py
-/usr/bin/nvidia-smi

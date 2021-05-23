5265111d55ab10b873f48158dcdc440f71b62f24
Marvin Peng <35765640+pengbohua@users.noreply.github.com>
2021-05-23 20:19:40 +0800

    add finetune

---
+++ elastic2.py
@@ -0,0 +1,33 @@
+from skimage.filters import gaussian
+
+def elastic_transform_v2(image, severity=1):
+    IMSIZE = 32
+    c = [(IMSIZE*0, IMSIZE*0, IMSIZE*0.08),
+         (IMSIZE*0.05, IMSIZE*0.2, IMSIZE*0.07),
+         (IMSIZE*0.08, IMSIZE*0.06, IMSIZE*0.06),
+         (IMSIZE*0.1, IMSIZE*0.04, IMSIZE*0.05),
+         (IMSIZE*0.1, IMSIZE*0.03, IMSIZE*0.03)][severity - 1]
+
+    image = np.array(image, dtype=np.float32) / 255.
+    shape = image.shape
+    shape_size = shape[:2]
+
+    # random affine
+    center_square = np.float32(shape_size) // 2
+    square_size = min(shape_size) // 3
+    pts1 = np.float32([center_square + square_size,
+                       [center_square[0] + square_size, center_square[1] - square_size],
+                       center_square - square_size])
+    pts2 = pts1 + np.random.uniform(-c[2], c[2], size=pts1.shape).astype(np.float32)
+    M = cv2.getAffineTransform(pts1, pts2)
+    image = cv2.warpAffine(image, M, shape_size[::-1], borderMode=cv2.BORDER_REFLECT_101)
+
+    dx = (gaussian(np.random.uniform(-1, 1, size=shape[:2]),
+                   c[1], mode='reflect', truncate=3) * c[0]).astype(np.float32)
+    dy = (gaussian(np.random.uniform(-1, 1, size=shape[:2]),
+                   c[1], mode='reflect', truncate=3) * c[0]).astype(np.float32)
+    dx, dy = dx[..., np.newaxis], dy[..., np.newaxis]
+
+    x, y, z = np.meshgrid(np.arange(shape[1]), np.arange(shape[0]), np.arange(shape[2]))
+    indices = np.reshape(y + dy, (-1, 1)), np.reshape(x + dx, (-1, 1)), np.reshape(z, (-1, 1))
+    return np.clip(map_coordinates(image, indices, order=1, mode='reflect').reshape(shape), 0, 1) * 255

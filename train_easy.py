2448530cced1dbbc9d9b56d7d33c4456f19f1633
Marvin Peng <35765640+pengbohua@users.noreply.github.com>
2021-05-27 13:28:13 +0800

    add train bar

--- train_easy.py
+++ train_easy.py
@@ -286,7 +286,7 @@ def train(net, data_loader, train_optimizer, epoch, args):
     adjust_learning_rate(train_optimizer, epoch, args)
     data_loader.sampler.set_epoch(epoch)
 
-    total_loss, total_num = 0.0, 0
+    total_loss, total_num, train_bar = 0.0, 0, tqdm(data_loader)
     for im_1, im_2, img_n in data_loader:
         im_1, im_2, img_n = im_1.cuda(non_blocking=True), im_2.cuda(non_blocking=True), img_n.cuda(non_blocking=True)
         loss = net(im_1, im_2, img_n)
@@ -297,6 +297,9 @@ def train(net, data_loader, train_optimizer, epoch, args):
 
         total_num += data_loader.batch_size
         total_loss += loss.item() * data_loader.batch_size
+        train_bar.set_description(
+            'Train Epoch: [{}/{}], lr: {:.6f}, Loss: {:.4f}'.format(epoch, args.epochs, train_optimizer.param_groups[0]['lr'],
+                                                                    total_loss / total_num))
 
     return total_loss / total_num
 
@@ -348,6 +351,7 @@ def test(net, memory_data_loader, test_data_loader, epoch, args):
             pred_labels = knn_predict(feature, feature_bank, feature_labels, classes, args.knn_k, args.knn_t)
             total_num += data.size(0)
             total_top1 += (pred_labels.argmax(dim=1, keepdim=True) == target).float().sum().item()
+            test_bar.set_description('Test Epoch: [{}/{}] Acc@1:{:.2f}%'.format(epoch, args.epochs, total_top1 / total_num * 100))
 
     return total_top1 / total_num * 100
 

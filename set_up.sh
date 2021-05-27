4d5cb0a38ddeab11d8055aa7536b1560e34f6b6f
Marvin Peng <35765640+pengbohua@users.noreply.github.com>
2021-05-27 13:31:00 +0800

    fix

--- set_up.sh
+++ set_up.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --mail-type=ALL # required to send email notifcations
 #SBATCH --mail-user=bp1119 # required to send email notifcations - please replace <your_username> with your college login name or email address
+#SBATCH -w cloud-vm-
 export PATH=/vol/bitbucket/bohua/miniconda3/bin/:$PATH
 source activate
-CUDA_VISIBLE_DEVICES=0 python -m -nproc_per_node=1 torch.distributed.launch train_v4.py
+CUDA_VISIBLE_DEVICES=0,1 python -m  torch.distributed.launch -nproc_per_node=2 train_easy.py --batch_size=100 --results-dir='./result_easy'
 /usr/bin/nvidia-smi
 uptime
